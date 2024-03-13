cc8112c658289dc65f10f67bf3d23bc95c208100
Kuhu Bhatnagar <kuhubhatnagar19@gmail.com>
2024-03-13 23:22:29 +0530

    testimonial slider implementation using js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const bars = [...document.querySelectorAll('.bar')];
 bars.forEach((bar,index) => {
     const selectedBar = document.querySelector(`.bar${index+1}`);
@@ -12,3 +11,35 @@ bars.forEach((bar,index) => {
     })
 });
 
+// customer testimonials slider 
+const testimonialImage = [...document.querySelectorAll('.testimonial-slide-image')];
+const reviews = [...document.querySelectorAll('.customer-review')];
+const testimonialSliderBars = [...document.querySelectorAll('.testimonial-slider-bar')];
+const nextSlideBtn = document.querySelector('.next-slide-btn');
+
+const image = document.querySelector('.testimonial-slide-image');
+const review = document.querySelector('.customer-review');
+testimonialSliderBars.forEach((bar,index) => {
+    bar.addEventListener('click',()=>{
+        // console.log('bar clicked : ',index+1);
+        
+        image.src = testimonialImage[index].src;
+        console.log(index ,' clicked', testimonialImage[index].src)
+        review.innerHTML = reviews[index].innerHTML;
+    })
+});
+let index=0;
+nextSlideBtn.addEventListener('click',()=>{
+    if(index == testimonialSliderBars.length)
+       {
+        index = 0;
+        image.src = testimonialImage[index].src;
+        review.innerHTML = reviews[index].innerHTML;
+       }
+    else{
+    image.src = testimonialImage[index].src;
+    console.log(index ,' clicked', testimonialImage[index].src)
+    review.innerHTML = reviews[index].innerHTML;
+    index++;
+}
+})
